864e812a3e789d27e502482a17177c8508edbe90
alexkorsky <koral7@yahoo.com>
2021-03-23 00:17:14 -0400

    main

--- src/main.cpp
+++ src/main.cpp
@@ -34,11 +34,21 @@ int main() {
   uWS::Hub h;
 
   PID pid;
+
+  bool TWIDDLE = false;
+
   /**
    * TODO: Initialize the pid variable.
    */
 
-  h.onMessage([&pid](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, 
+  if (TWIDDLE)
+	  pid.Init(0, 0, 0);
+  else
+	  pid.Init(0.449131,  7.71561,  0);
+
+  	  //pid.Init(1.42, 11.168, 0);
+
+  h.onMessage([&pid, &TWIDDLE](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length,
                      uWS::OpCode opCode) {
     // "42" at the start of the message means there's a websocket message event.
     // The 4 signifies a websocket message
@@ -64,9 +74,17 @@ int main() {
            *   Maybe use another PID controller to control the speed!
            */
           
+          pid.UpdateError(cte);
+
+          if (TWIDDLE)
+        	  pid.Twiddle(cte);
+
+          steer_value = pid.TotalError();
+
+
           // DEBUG
-          std::cout << "CTE: " << cte << " Steering Value: " << steer_value 
-                    << std::endl;
+          if (!TWIDDLE)
+        	  std::cout << "CTE: " << cte << " Steering Value: " << steer_value << std::endl;
 
           json msgJson;
           msgJson["steering_angle"] = steer_value;
