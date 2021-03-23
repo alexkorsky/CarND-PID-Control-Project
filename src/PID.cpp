82414bfb12a7d751c02a805952ab02bce53c8601
alexkorsky <koral7@yahoo.com>
2021-03-23 00:16:59 -0400

    PID class

--- src/PID.cpp
+++ src/PID.cpp
@@ -1,5 +1,7 @@
 #include "PID.h"
 
+using std::string;
+
 /**
  * TODO: Complete the PID class. You may add any additional desired functions.
  */
@@ -8,23 +10,198 @@ PID::PID() {}
 
 PID::~PID() {}
 
-void PID::Init(double Kp_, double Ki_, double Kd_) {
+void PID::Init(double Kp, double Kd, double Ki)
+{
   /**
    * TODO: Initialize PID coefficients (and errors, if needed)
    */
 
+	Kp_ = Kp;
+	Kd_ = Kd;
+	Ki_ = Ki;
+
+	p_error_ = 0;
+	i_error_ = 0;
+	d_error_ = 0;
+
+
+	// TWIDDLE:
+
+	dp_[0] = 0.1;
+	dp_[1] = 1;
+	dp_[2] = 0.001;
+
+//	dp_[0] = 1;
+//	dp_[1] = 1;
+//	dp_[2] = 1;
+
+	best_error_ = std::numeric_limits<double>::max();
+	idx_ = 0;
+	state_ = UPDATE_CURRENT_COEFFICIENT;
 }
 
-void PID::UpdateError(double cte) {
+void PID::UpdateError(double cte)
+{
   /**
    * TODO: Update PID errors based on cte.
    */
 
+	  d_error_ = cte - p_error_;
+	  p_error_ = cte;
+	  i_error_ += cte;
 }
 
-double PID::TotalError() {
+double PID::TotalError()
+{
   /**
    * TODO: Calculate and return the total error
    */
-  return 0.0;  // TODO: Add your total error calc here!
+
+
+//	double result;
+//
+//
+//	// progressive PID
+//    if (fabs(p_error_) < 0.2)
+//    	result = 0;
+//    else if (fabs(p_error_) < 2) // 70% steer
+//    	result = -Kp_ * 0.7 * p_error_ - Kd_ * 0.7 * d_error_ - Ki_ * i_error_;
+//    else if (fabs(p_error_) < 5) // 80% steer
+//    	result = -Kp_ * 0.8 * p_error_ - Kd_ * 0.8 * d_error_ - Ki_ * i_error_;
+//    else if (fabs(p_error_) < 8) // 90% steer
+//    	result = -Kp_ * 0.9 * p_error_ - Kd_ * 0.9 * d_error_ - Ki_ * i_error_;
+//    else // 100%
+//    	result = -Kp_ * 1.0 * p_error_ - Kd_ * 1.0 * d_error_ - Ki_ * i_error_;
+//
+//    if(result > 1.0)
+//    	result = 1.0;
+//    else if(result < -1.0)
+//    	result = -1.0;
+//
+//
+//    return result;
+
+
+    // non-progressive PID
+    return -Kp_ * p_error_ - Ki_ * i_error_ - Kd_ * d_error_;
+
+}
+
+void PID::Twiddle(double cte)
+{
+	double p[] = {Kp_, Kd_, Ki_};
+
+
+	std::string  STATE_STR;
+	if (state_ == UPDATE_CURRENT_COEFFICIENT)
+	{
+		STATE_STR = "UPDATE_CURRENT_COEFFICIENT";
+	}
+	else if (state_ ==  EVALUATE_NEXT_ERROR)
+	{
+		STATE_STR = "EVALUATE_NEXT_ERROR";
+	}
+	else if (state_ ==  REEVALUATE_NEXT_ERROR)
+	{
+		STATE_STR = "REEVALUATE_NEXT_ERROR";
+	}
+	else if (state_ == SWITCH_TO_NEXT_COEFFICIENT)
+	{
+		STATE_STR = "SWITCH_TO_NEXT_COEFFICIENT";
+	}
+
+	std::string  INDEX_STR;
+	if (idx_ == 0)
+	{
+		INDEX_STR = "P";
+	}
+	else if (idx_ ==  1)
+	{
+		INDEX_STR = "D";
+	}
+	else if (idx_ ==  2)
+	{
+		INDEX_STR = "I";
+	}
+
+	std::cout << "----------------------------------------------------------" << std::endl;
+    std::cout << "p_error_: " << p_error_ << std::endl;
+    std::cout << "d_error_: " << d_error_ << std::endl;
+    std::cout << "i_error_: " << i_error_ << std::endl;
+
+    std::cout << "CTE: " << cte << std::endl;
+
+    std::cout << "state_: " << STATE_STR << std::endl;
+    std::cout << "idx_: " << INDEX_STR  << std::endl;
+
+
+    std::cout << "BestError: " << best_error_ << std::endl;
+
+	if ((fabs(dp_[0]) + fabs(dp_[1]) + fabs(dp_[2])) > TOL)
+	{
+
+		bool getNextTelemetry = false;
+
+		while (!getNextTelemetry)
+		{
+			if (state_ == UPDATE_CURRENT_COEFFICIENT)
+			{
+				p[idx_] += dp_[idx_];
+
+				state_ = EVALUATE_NEXT_ERROR;
+				getNextTelemetry = true;
+			}
+			else if (state_ ==  EVALUATE_NEXT_ERROR)
+			{
+				if (fabs(cte) < best_error_)
+				{
+					best_error_ = fabs(cte);
+					dp_[idx_] *= 1.1;
+
+					state_ = SWITCH_TO_NEXT_COEFFICIENT;
+				}
+				else
+				{
+					p[idx_] -= 2 * dp_[idx_];
+
+					state_ = REEVALUATE_NEXT_ERROR;
+					getNextTelemetry = true;
+				}
+			}
+			else if (state_ ==  REEVALUATE_NEXT_ERROR)
+			{
+				if (fabs(cte) < best_error_)
+				{
+					best_error_ = fabs(cte);
+					dp_[idx_] *= 1.1;
+				}
+				else
+				{
+					p[idx_] += dp_[idx_];
+					dp_[idx_] *= 0.9;
+				}
+
+				state_ = SWITCH_TO_NEXT_COEFFICIENT;
+			}
+
+
+			if (state_ == SWITCH_TO_NEXT_COEFFICIENT)
+			{
+				idx_ = (idx_ + 1) % 2;
+
+
+				state_ = UPDATE_CURRENT_COEFFICIENT;
+
+				if (idx_== 0)
+					getNextTelemetry = true;
+			}
+		}
+
+		Kp_ = p[0];
+		Kd_ = p[1];
+		Ki_ = p[2];
+
+		std::cout << "Adjusters [ dp[0]: " << dp_[0] << " | dp[1]: " << dp_[1] << " | dp[2]: " << dp_[2] << "]" << std::endl;
+		std::cout << "TWIDDLE [ Kp: " << Kp_ << " | Kd: " << Kd_ << " | Ki: " << Ki_ << "]" << std::endl;
+	}
 }
--- src/PID.h
+++ src/PID.h
@@ -1,8 +1,21 @@
 #ifndef PID_H
 #define PID_H
 
+#include <math.h>
+#include <limits>
+#include <iostream>
+
+
 class PID {
  public:
+
+  const double TOL = 0.00001;
+
+  const int UPDATE_CURRENT_COEFFICIENT = 0;
+  const int EVALUATE_NEXT_ERROR = 1;
+  const int REEVALUATE_NEXT_ERROR = 2;
+  const int SWITCH_TO_NEXT_COEFFICIENT = 3;
+
   /**
    * Constructor
    */
@@ -17,13 +30,21 @@ class PID {
    * Initialize PID.
    * @param (Kp_, Ki_, Kd_) The initial PID coefficients
    */
-  void Init(double Kp_, double Ki_, double Kd_);
+  void Init(double Kp, double Kd, double Ki);
 
   /**
    * Update the PID error variables given cross track error.
    * @param cte The current cross track error
    */
   void UpdateError(double cte);
+
+  /**
+   * Twiddle the coefficients given the performance of the next step
+   * with respect to the error.
+   * @param cte The current cross track error
+   */
+  void Twiddle(double cte);
+
 
   /**
    * Calculate the total PID error.
@@ -35,16 +56,27 @@ class PID {
   /**
    * PID Errors
    */
-  double p_error;
-  double i_error;
-  double d_error;
+  double p_error_;
+  double i_error_;
+  double d_error_;
+
+  double total_error_;
 
   /**
    * PID Coefficients
    */ 
-  double Kp;
-  double Ki;
-  double Kd;
+  double Kp_;
+  double Ki_;
+  double Kd_;
+
+  /*
+   * Twiddle:
+   */
+
+  double dp_[3];
+  double best_error_;
+  int idx_;
+  int state_;
 };
 
 #endif  // PID_H
